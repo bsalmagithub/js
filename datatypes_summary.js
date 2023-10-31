331fa3f063e23f5f7de79ae8b81e5074f14f4849
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-10-31 21:46:12 +0000

    datatypes summary

---
+++ datatypes_summary.js
@@ -0,0 +1,13 @@
+// it will help interview also
+//in official documentaion we have only 2 types of data types 1)Primitive and 2)Non-primitive or refernces
+//how data will store ? and how can we access the data in memory in that base we have two types
+
+// Primitive 
+// we have 7 categories: Srting, Number, Boolean, null(empty not 0 not empty string ), undefined, symbol(value is unique), BitInt 
+
+
+//Refernce (Non primitive)
+//Array , Objects, Functions
+
+
+
