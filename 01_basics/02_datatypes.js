b9c84711732d426a18e481885f148222823cb909
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-10-31 18:19:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer veresion
+
+// alert(3 + 3)// we are using node.js, not browser 
+
+// console.log( 3 +3); console.log("Salma")  code should be reabability should be high  so don't write like this
+
+
+//primitive data types
+let name="Salma" 
+let age=18 
+let isLoggedIn= false 
+// string  => "" ''
+//number => to power 53 range z
+// bigint if your number is very big
+//boolean => true / false  yes or no 
+//null  is standalone value  (TYPE Is object)
+//undefined => value not assign to variable (TYPE is undefined)
+// symbol => unique 
+
+// another is object
+
+console.log(typeof "salma")
+
