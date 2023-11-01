eb4477638193d1e7c44c33c6eee0532a1f81ea9f
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-01 11:50:40 +0000

    datatype summary

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -5,9 +5,43 @@
 // Primitive 
 // we have 7 categories: Srting, Number, Boolean, null(empty not 0 not empty string ), undefined, symbol(value is unique), BitInt 
 
+const score=100  
+const scoreValue= 100.3
+
+const isLoggedIn=false 
+const outsideTemp= null // nothing 
+const userEmail=undefined // undefined
+
+const id= Symbol("123")
+const anotherId= Symbol('123')
+console.log(id === anotherId); // false
+
+const bigNumber= 333333333333333n
+
+
 
 //Refernce (Non primitive)
 //Array , Objects, Functions
 
 
+const heros= ["salma", "rakshith", "Abbu", "Ammi"]//Arrays
+console.log(typeof heros)
 
+let myObj={
+    name: "salma",
+    age:22,
+}
+console.log(typeof myObj);
+//Object
+
+//there are so many functuion now we learn Variable function
+
+const myFunction=function(){
+    console.log('Hello World')
+}
+console.log(typeof myFunction)// data type is Function it written. but we say Object function
+// all non primitive data type is Function
+
+
+//link for datatypes 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
