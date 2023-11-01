3d0319d1b946348c5aad046d0744c753a44e8640
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-01 11:56:24 +0000

    datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,36 +1,35 @@
+
+
 let score=33
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInString= String(score)
+console.log(typeof valueInString)
+
+
 let scores= "33abc"
+//convert Number
+let valueInNumber= Number(scores)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //NaN is a special type 
+
+
 let scoress= null 
 let scoress_number= Number(scoress)
+console.log(scoress)
+console.log(scoress_number) // value of null is (0) zero
 
 let score_undefined= undefined 
 let undefinedNumber= Number(score_undefined)
+console.log(typeof(undefinedNumber)) // type is Number
+console.log(undefinedNumber)// value is NaN
+
 
 let score_boolean= true
 let boolean_number= Number(score_boolean)
-
- console.log(typeof score)
- console.log(typeof (score))
-
- let valueInString= String(score)
- console.log(typeof valueInString)
-
- //convert Number
- let valueInNumber= Number(scores)
- console.log(typeof valueInNumber)
- console.log(valueInNumber) //NaN is a special type 
-
-
- console.log(scoress)
- console.log(scoress_number) // value of null is (0) zero
-
-
- console.log(typeof(undefinedNumber)) // type is Number
- console.log(undefinedNumber)// value is NaN
-
-
- console.log(typeof(boolean_number)) //type is number
- console.log(boolean_number) //value is 1 
+console.log(typeof(boolean_number)) //type is number
+console.log(boolean_number) //value is 1 
 
  //"33" =>33
  //"33abc" => NaN Not an Number but type is NUMBER
