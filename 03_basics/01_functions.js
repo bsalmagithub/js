05bd16fd8397ffcbd2b4dccc04bcc5af543158e9
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-15 17:25:09 +0000

    scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -85,4 +85,6 @@ function returnSecondValue(getArray){
     return getArray[1]
 }
 //console.log(returnSecondValue(myNewArray));//400
-console.log(returnSecondValue([200, 400, 100, 600]))//400
+//console.log(returnSecondValue([200, 400, 100, 600]))//400
+
+
