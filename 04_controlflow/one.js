9721c8b1476f358dc27027a9188038984aa5b35e
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-18 12:52:25 +0000

    controlflow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,16 @@
+// CONTROL flow or LOGI flow
+//if statement is first control flow
+
+const isUserLoggedIn=true
+const temperature=41
+
+if (temperature){  // it is true 
+    console.log("less than 50"); // this will be execute
+}
+else{
+    console.log("temperature is greater than 50"); //false this will be execute
+}
+console.log("execute");
+
+// comparetive oparators 
+// < less than , > greater than,  <= less than equal, >= greater than equal, == equal , === strick equal it check data type also,  != not eqaul , !==  = assignment operator not equal
