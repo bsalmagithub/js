b55aa7110ee452df65bd9d038a51d0c55d2e8b92
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-18 14:38:44 +0000

    controlflow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -4,13 +4,27 @@
 const isUserLoggedIn=true
 const temperature=41
 
-if (temperature){  // it is true 
+/*if (temperature){  // it is true 
     console.log("less than 50"); // this will be execute
 }
 else{
     console.log("temperature is greater than 50"); //false this will be execute
 }
-console.log("execute");
+console.log("execute");*/
 
 // comparetive oparators 
 // < less than , > greater than,  <= less than equal, >= greater than equal, == equal , === strick equal it check data type also,  != not eqaul , !==  = assignment operator not equal
+
+/*const score=200
+if (score > 100) {
+    const power= "fly" // if we use var keyword it will get out of the scope also but we don't want so use let or const
+    console.log(`User power: ${power}`); // User power: fly
+}
+// console.log(`User power: ${power}`); get error because it is output our scope
+*/
+
+// shot hand notation
+const balance=1000
+//this is called implicit scope it assume that there is scope we can't write multiple line some people write they instead of ; they put , basically don't write like this
+//if (balance >500) console.log("test"); //test
+
