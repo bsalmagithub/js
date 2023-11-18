f7e8b78d465fdccd77b7d9244a0f36a8ba6657b5
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-18 17:58:25 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+// basic syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//control flow
+const month=3
+switch (month){
+    case 1:
+        console.log('January');
+        break;
+    case 2:                         // copy the code which you want write exacly same syntax is shift + alt + down arrow
+        console.log('Feb');
+        break;
+    case 3:
+        console.log('March');   //March
+        break;
+    
+    case 4:
+        console.log('April');
+        break;
+    default:
+        console.log('default case match');
+        break;
+}
