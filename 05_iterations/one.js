947a7d1c85b69f236b2272d09e38dd0acfcea362
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-21 07:26:30 +0000

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -9,7 +9,49 @@
 
 for (let i= 0; i< 10; i++) {      // we change variable name in all places double tap select the variable and click ctrl + d which you want change
     const element = i;
-    console.log(element);
-    
+    if (element == 5) {
+        //console.log('5 is best number');
+    }
+    //console.log(element);
 }
 // console.log(element);  here we get error because it is delcared in scope we can't access out of scope
+
+
+//nested loop
+for (let i = 0; i <= 10; i++){
+    //console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+        //console.log(`Inner loop value: ${j} and outer loop ${i}`);
+        //console.log(i+ "*" + j + " = " + i * j);
+    }
+}
+
+
+//array
+const myArray=["flash","batman","superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+
+//keywords
+//break and continue  
+
+// for (let index = 1; index < 20; index++) {
+//     if (index == 5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+for (let index = 1; index < 20; index++) {
+    if (index == 5){
+        console.log(`Detected 5`);
+        continue  // one time ke liye ignore
+    }
+    console.log(`value of i is ${index}`);
+    
+}
