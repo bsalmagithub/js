949fba5b183d53ad14800840d0994f64a32aeb8d
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-11-21 18:50:41 +0000

    loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,23 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {},{}]
+
+// for (const iterator of object) {        // iterator means object, arrays, string . object => which think we are talking 
+    
+// }
+
+const arr=[1,2,3,4,5]
+for (const num of arr){
+    console.log(num);
+
+}
+
+const greeting= "Hello World"
+for (const greet of greeting){
+    console.log(`Each char is ${greet}`);
+}
+
+
+//+++++++  maps  +++++++
+
