60913498b62f082d5b681487cd53b1338ec7236c
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-12-01 11:45:08 +0000

    maps

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -20,4 +20,30 @@ for (const greet of greeting){
 
 
 //+++++++  maps  +++++++
+// Maps => is object holds key-value pairs it give original insertion order of the keys. it is unique in the maps collection
 
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);  //myObject is not iterable
+    
+// }
+
+// object are iteratable but this is not way
+//for of loop is not working on objects
