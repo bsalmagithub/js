fa5cfe450c4250e583049abf9afba7136ebee57f
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-12-09 20:10:43 +0000

    promise

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,73 @@
+const promiseOne= new Promise( function (resolve, reject) {  //creating promise
+    //do an async task
+        //DB calls, cryptography, network call
+    setTimeout(function(){
+        console.log('Async task is completed');
+        resolve()       //if want resolve , call the resolve() method and its connected to the .this()
+    },1000)
+})
+//promise is created now it time to consume
+
+promiseOne.then(function(){       //.then(call back) is directly connected to the resolve
+    console.log('promise consumed');
+})
+
+// Async task is completed
+// promise consumed         output got
+
+
+// we can create without variable promise also because variable it not complusary
+
+new Promise(function ( resolve, reject){
+    setTimeout( function(){
+        console.log('Async task 2');
+        resolve()
+
+    },1000)
+}).then( function(){
+    console.log('Async 2 resolved');
+})
+// Async task 2
+// Async 2 resolved
+
+
+//third promise
+const promiseThree= new Promise( function( resolve, reject){
+    setTimeout(function(){
+        resolve({username:'Chai', email: 'javascriptcode.come'})        // here passing through resolve we can pass parameter , data object Array function etc
+
+    }, 1000)
+})
+
+promiseThree.then( function(user){
+    console.log(user);      //{ username: 'Chai', email: 'javascriptcode.come' }
+
+})
+
+//promise four
+const promiseFour= new Promise( function( resolve, reject){
+    setTimeout( function(){
+        let error= false   //true get error 
+        if (!error){
+            resolve({username:'salma',password: '123'})
+        }
+        else{
+            reject('ERROR: Something went wrong ')
+        }
+    }, 1000)
+})
+
+promiseFour
+.then((user) =>{
+    console.log(user);
+    return user.username
+})
+.then( (username)=>{      //chaining
+    console.log(username);
+})
+.catch( function(error){
+    console.log(error);
+}).finally( ()=>{
+    console.log('The promise is either resolved or reject');   //The promise is either resolved or reject
+})
+
