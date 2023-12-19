c12788dd9483584dda4fd245030665e8bfb5d39a
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-12-19 10:22:18 +0000

    oop

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,13 @@
+const user={
+    username:'salma',
+    loginCount: 8,
+    signedIn:true,
+
+    //method  etUserDetails
+    getUserDetails: function(){
+        console.log('Got user details from database');
+
+    }
+}
+console.log(user.username);
+console.log(user.getUserDetails()); //method should be run () not refernce
