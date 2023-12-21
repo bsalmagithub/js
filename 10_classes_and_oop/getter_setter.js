7a2c7ee8ebcbd67e4544b4474ff80e996bc049d8
bsalmagithub <132895881+bsalmagithub@users.noreply.github.com>
2023-12-21 09:17:40 +0000

    getter setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -4,17 +4,28 @@ class User{
         this.password=password
 
     }
+
     //getter
     get password(){
-        return this.password.toUpperCase()
+        // return this._password.toUpperCase()
+        return `${this._password}salma`
 
 
     }
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email=value
+    }
+
     //setter
     set password(value){
-        this.password=value.toUpperCase()
+        this._password=value.toUpperCase()
     }
 }
 const salma= new User('salma@123.com', '123')
 console.log(salma.password);
+console.log(salma.email);
 
